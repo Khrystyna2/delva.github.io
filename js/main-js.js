3baca58027bb0fb4eadf711a59a42fdd0e5d9331
Khrystyna Khozhai <l.xrustja@gmail.com>
2018-11-07 16:25:53 +0200

    header style

--- js/main-js.js
+++ js/main-js.js
@@ -1,7 +1,22 @@
 $(document).ready(function() {
 
-   $('#menu-dropDown').click(function() {
-        $(this).find('ul').slideToggle();
+   $('#menu-dropDown>div').click(function() {
+        $(this).next('ul').slideToggle();
+        $('#menu-dropDown').toggleClass('rotate');
    });
+  
+    $('#search>img').click(function() {
+        $('#search>input').toggleClass('open-input');  
+    });
+
+    $('.btn-menu').click(function() {
+        $('.menu').addClass('show-menu');
+   });
+
+   $('.btn-close').click(function() {
+        $('.menu').removeClass('show-menu');
+   });
+
     
 });
+
