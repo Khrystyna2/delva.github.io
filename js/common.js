afc402381e37cfa766a842ffed77a6467ffefbbc
v.monasteretskyy <v.monasteretskyy@gmail.com>
2018-11-13 08:45:52 +0200

    product page

--- js/common.js
+++ js/common.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
     // style select
     $(function () {
-        $('select').styler();
+        $('select, input[type="number"]').styler();
     });
     // catalog top filter
     $('.catgrid-links span').click(function () {
         $('.catgrid-links span').removeClass('active');
         $(this).addClass('active');
     });
-    $('.row-gr').click(function(){
+    $('.row-gr').click(function () {
         $('.catalog-products').addClass('rowproducts')
     });
-    $('.table-gr').click(function(){
+    $('.table-gr').click(function () {
         $('.catalog-products').removeClass('rowproducts')
     });
     // event slider
@@ -35,19 +35,55 @@ $(document).ready(function () {
                     slidesToScroll: 1,
                 }
             }
-            
+
         ]
     });
     // responsive filter
-    $('.left-filt .title').click(function(){
-       $(this).next().slideToggle(); 
+    $('.left-filt .title').click(function () {
+        $(this).next().slideToggle();
     });
     // footer sitemap
     if ($(window).width() < 768) {
-       $('footer .foot-sitemap p.title').click(function(){
-           $(this).toggleClass('rotatepic')
-           $('.foot-sitemap ul').slideToggle();
-       })
-     }
-     
+        $('footer .foot-sitemap p.title').click(function () {
+            $(this).toggleClass('rotatepic')
+            $('.foot-sitemap ul').slideToggle();
+        })
+    }
+    //  product slider
+    // event slider
+    $('.card-slider').slick({
+        arrows: false,
+        infinite: true,
+        dots:true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    });
+    
+    $('.bottom-prslider').slick({
+        arrows: true,
+        infinite: true,
+        dots:false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '0px',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    });
 });
